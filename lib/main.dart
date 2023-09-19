81756bdaecb016749be88f0fa40d11010fcb4cdf
is <inpvp1206@gmail.com>
2023-09-20 00:50:39 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyWidget());
+}
+
+final a = ["사고등록", "사고현황", "통계"];
+final controller = PageController(initialPage: 0);
+int i = 0;
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text(a[i])),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            Icon(Icons.abc),
+            Icon(Icons.baby_changing_station),
+            Icon(Icons.cabin)
+          ],
+          selectedIndex: i,
+          onDestinationSelected: (value) {},
+        ),
+        body: PageView(
+          onPageChanged: (int page) {
+            setState(() {
+              i = page.toInt();
+            });
+          },
+          controller: controller,
+          children: [
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.red,
+            )
+          ],
+        ),
+      ),
+    );
+    ;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,19 +60,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: PageController(initialPage: 0),
-      children: [
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.red,
-        )
-      ],
-    );
+    return Container();
   }
 }
+
+int b = controller.page!.toInt();
